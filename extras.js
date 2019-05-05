2060342c0d4ab9859e150e9bc935d2ee124c6078
Chris <kristijan.ros@gmail.com>
2019-05-05 20:02:07 +0200

    Update extras.js

--- extras.js
+++ extras.js
@@ -1,5 +1,16 @@
-var shortcuts = document.getElementsByClassName("text-primary display-7");
-console.log("HI", shortcuts)
-function slideAndOpen(element) {
-
-}
+const buttons = $(".tab-button")[0];
+console.log(buttons)
+$(document).ready(function() {
+  $('.text-primary display-7').on('click', function() {
+    $("html, body").animate({scrollTop: $($("#tab1-i")).position().top}, 1000);
+    if (this.textContent == "Instalacija i privatnost") {
+      buttons[1].click();
+    } else if (this.textContent == "Mogućnosti") {
+      buttons[2].click();
+    } else if (this.textContent == "Dolazi uskoro") {
+      buttons[3].click();
+    } else {
+      buttons[4].click();
+    }
+  });
+});
