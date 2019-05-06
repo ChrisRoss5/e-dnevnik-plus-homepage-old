4772a42d589b26279e4100e6d04ccddd7aafcb67
Chris <kristijan.ros@gmail.com>
2019-05-06 09:53:57 +0200

    Update extras.js

--- extras.js
+++ extras.js
@@ -14,10 +14,12 @@ window.addEventListener("load", function () {
     }
     return false;
   });
+  var share = $("amp-social-share");
+  console.log(share)
   $("amp-social-share").on('click', function() {
     var type = $(this).attr("type")
     if (type == "email") {
-      console.log("HI");
+      console.log("HI2");
       window.location.assign("mailto:?subject=Odli%C4%8Dna%20ekstenzija%20za%20Chrome%20%E2%80%94%20e-Dnevnik%20Plus&body=https%3A//chrome.google.com/webstore/detail/e-dnevnik-plus/bcnccmamhmcabokipgjechdeealcmdbe");
     } else if (type == "facebook") {
       window.location.assign("https://www.facebook.com/sharer/sharer.php?u=https%3A//chrome.google.com/webstore/detail/e-dnevnik-plus/bcnccmamhmcabokipgjechdeealcmdbe");
