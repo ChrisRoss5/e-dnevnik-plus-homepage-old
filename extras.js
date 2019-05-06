c9374e353920d9cbd8e7b1cdb39f1be35a409e81
Chris <kristijan.ros@gmail.com>
2019-05-06 12:12:06 +0200

    Update extras.js

--- extras.js
+++ extras.js
@@ -14,19 +14,4 @@ window.addEventListener("load", function () {
     }
     return false;
   });
-  $("amp-social-share").on('click', function() {
-
-    return false
-/*     var type = $(this).attr("type")
-    if (type == "email") {
-      window.location.assign("mailto:?subject=Odli%C4%8Dna%20ekstenzija%20za%20Chrome%20%E2%80%94%20e-Dnevnik%20Plus&body=https%3A//chrome.google.com/webstore/detail/e-dnevnik-plus/bcnccmamhmcabokipgjechdeealcmdbe");
-    } else if (type == "facebook") {
-      window.open("https://www.facebook.com/sharer/sharer.php?u=https%3A//chrome.google.com/webstore/detail/e-dnevnik-plus/bcnccmamhmcabokipgjechdeealcmdbe");
-    } else if (type == "linkedin") {
-      window.open("https://www.linkedin.com/shareArticle?mini=true&url=https%3A//chrome.google.com/webstore/detail/e-dnevnik-plus/bcnccmamhmcabokipgjechdeealcmdbe&title=Odli%C4%8Dna%20ekstenzija%20za%20Chrome%20%E2%80%94%20e-Dnevnik%20Plus.&summary=Napredne%20postavke%20e-Dnevnika%20za%20u%C4%8Denike%20i%20roditelje&source=ednevnik.plus");
-    } else {
-      window.open("https://twitter.com/home?status=Odli%C4%8Dna%20ekstenzija%20za%20Chrome%20%E2%80%94%20e-Dnevnik%20Plus%3A%0Ahttps%3A//chrome.google.com/webstore/detail/e-dnevnik-plus/bcnccmamhmcabokipgjechdeealcmdbe");
-    }
-    return false; */
-  });
 });
