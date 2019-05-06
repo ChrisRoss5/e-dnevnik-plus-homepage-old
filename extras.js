13337bce71610fe9ee0d4b1513cd076e890cf44b
Chris <kristijan.ros@gmail.com>
2019-05-06 09:18:00 +0200

    Update extras.js

--- extras.js
+++ extras.js
@@ -1,7 +1,7 @@
 window.addEventListener("load", function () {
   const buttons = $(".tab-button");
   console.log(buttons)
-  $('.text-primary display-7').on('click', function() {
+  $('.text-primary').on('click', function() {
     console.log("HI");
     $("html, body").animate({scrollTop: $($("#tab1-i")).position().top}, 1000);
     $(".close-sidebar")[0].click();
@@ -17,13 +17,13 @@ window.addEventListener("load", function () {
     return false;
   });
   $("amp-social-share").on('click', function() {
-    console.log("HII", this.type)
-    if (this.type == "email") {
+    console.log("HII", this.attr("type"))
+    if (this.attr("type") == "email") {
       console.log("HI2");
       window.location.assign("mailto:?subject=Odli%C4%8Dna%20ekstenzija%20za%20Chrome%20%E2%80%94%20e-Dnevnik%20Plus&body=https%3A//chrome.google.com/webstore/detail/e-dnevnik-plus/bcnccmamhmcabokipgjechdeealcmdbe");
-    } else if (this.type == "facebook") {
+    } else if (this.attr("type") == "facebook") {
       window.location.assign();
-    } else if (this.type == "linkedin") {
+    } else if (this.attr("type") == "linkedin") {
       window.location.assign();
     } else {
       window.location.assign();
